0802641fe47ed97922d8f7490a0238dcca521b68
fhijo15 <33266880+fhijo15@users.noreply.github.com>
2020-01-16 01:11:22 +0100

    Update caption.py

--- caption.py
+++ caption.py
@@ -1,3 +1,69 @@
+import time
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torchvision.transforms as transforms
+from torch import nn
+from torch.nn.utils.rnn import pack_padded_sequence
+from models import DecoderWithAttention
+from datasets import *
+from utils import *
+from nltk.translate.bleu_score import corpus_bleu
+
+data_folder = 'final_dataset'  # folder with data files saved by create_input_files.py
+data_name = 'coco_5_cap_per_img_5_min_word_freq'  # base name shared by data files
+checkpoint_file ='model_poids'#'checkpoint_coco_5_cap_per_img_5_min_word_freq.pth.tar'#'BEST_9checkpoint_coco_5_cap_per_img_5_min_word_freq.pth.tar'#'checkpoint_coco_5_cap_per_img_5_min_word_freq.pth.tar'# 'BEST_34checkpoint_coco_5_cap_per_img_5_min_word_freq.pth.tar'  # model checkpoint
+
+word_map_file = 'WORDMAP_coco_5_cap_per_img_5_min_word_freq.json'  # word map, ensure it's the same the data was encoded with and the model was trained with
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # sets device for model and PyTorch tensors
+cudnn.benchmark = True  # set to true only if inputs to model are fixed size; otherwise lot of computational overhead
+emb_dim = 1024  # dimension of word embeddings
+attention_dim = 1024  # dimension of attention linear layers
+decoder_dim = 1024  # dimension of decoder RNN
+dropout = 0.5
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # sets device for model and PyTorch tensors
+cudnn.benchmark = True  # set to true only if inputs to model are fixed size; otherwise lot of computational overhe
+
+# Load model
+# Read word map
+word_map_file = os.path.join(data_folder, 'WORDMAP_' + data_name + '.json')
+with open(word_map_file, 'r') as j:
+    word_map = json.load(j)
+
+torch.nn.Module.dump_patches = True
+decoder = DecoderWithAttention(attention_dim=attention_dim,
+                                       embed_dim=emb_dim,
+                                       decoder_dim=decoder_dim,
+                                       vocab_size=len(word_map),
+                                       dropout=dropout)
+decoder.load_state_dict(torch.load(checkpoint_file))
+decoder = decoder.to(device)
+decoder.eval()
+print(type(decoder))
+
+
+def main():
+    """
+    Training and validation.
+    """
+
+    global best_bleu4, epochs_since_improvement, checkpoint, start_epoch,data_name, word_map
+
+    # Read word map
+    word_map_file = os.path.join(data_folder, 'WORDMAP_' + data_name + '.json')
+    with open(word_map_file, 'r') as j:
+        word_map = json.load(j)
+
+
+    # Custom dataloaders
+    train_loader = torch.utils.data.DataLoader(
+        CaptionDataset(data_folder, data_name, 'TRAIN'),
+        batch_size=batch_size, shuffle=True, num_workers=workers, pin_memory=True)
+    
+        # One epoch's training
+    caption(train_loader=train_loader,decoder=decoder,criterion_ce = criterion_ce,criterion_dis=criterion_dis,decoder_optimizer=decoder_optimizer,epoch=epoch)
+
+
 def caption(train_loader, decoder, criterion_ce, criterion_dis, decoder_optimizer, epoch):
     """
     Performs one epoch's training.
@@ -43,4 +109,8 @@ def caption(train_loader, decoder, criterion_ce, criterion_dis, decoder_optimize
 	print(targets)
 	
 
+	
 
+	
+if __name__ == '__main__':
+    main()
