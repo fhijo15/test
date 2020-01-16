55c42e362d61990b7cd811a154d7a7f2744dab2e
fhijo15 <33266880+fhijo15@users.noreply.github.com>
2020-01-16 01:05:50 +0100

    Update caption.py

--- caption.py
+++ caption.py
@@ -9,20 +9,13 @@ def caption(train_loader, decoder, criterion_ce, criterion_dis, decoder_optimize
     :param epoch: epoch number
     """
 
-    decoder.train()  # train mode (dropout and batchnorm is used)
-
-    batch_time = AverageMeter()  # forward prop. + back prop. time
-    data_time = AverageMeter()  # data loading time
-    losses = AverageMeter()  # loss (per word decoded)
-    top5accs = AverageMeter()  # top5 accuracy
-
-    start = time.time()
-
     # Batches
     for i, (imgs, caps, caplens) in enumerate(train_loader):
         data_time.update(time.time() - start)
 
         # Move to GPU, if available
+	print(type(imgs))
+	print(imgs)
         imgs = imgs.to(device)
         caps = caps.to(device)
         caplens = caplens.to(device)
@@ -43,39 +36,11 @@ def caption(train_loader, decoder, criterion_ce, criterion_dis, decoder_optimize
 
         # Remove timesteps that we didn't decode at, or are pads
         # pack_padded_sequence is an easy trick to do this
-        scores, _ = pack_padded_sequence(scores, decode_lengths, batch_first=True)
-        targets, _ = pack_padded_sequence(targets, decode_lengths, batch_first=True)
-
-        # Calculate loss
-        loss_d = criterion_dis(scores_d,targets_d.long())
-        loss_g = criterion_ce(scores, targets)
-        loss = loss_g + (10 * loss_d)
-        
-        # Back prop.
-        decoder_optimizer.zero_grad()
-        loss.backward()
+        scores= pack_padded_sequence(scores, decode_lengths, batch_first=True)[0]
+        targets = pack_padded_sequence(targets, decode_lengths, batch_first=True)[0]
 	
-        # Clip gradients when they are getting too large
-        torch.nn.utils.clip_grad_norm_(filter(lambda p: p.requires_grad, decoder.parameters()), 0.25)
+	print(scores)
+	print(targets)
+	
 
-        # Update weights
-        decoder_optimizer.step()
 
-        # Keep track of metrics
-        top5 = accuracy(scores, targets, 5)
-        losses.update(loss.item(), sum(decode_lengths))
-        top5accs.update(top5, sum(decode_lengths))
-        batch_time.update(time.time() - start)
-
-        start = time.time()
-
-        # Print status
-        if i % print_freq == 0:
-            print('Epoch: [{0}][{1}/{2}]\t'
-                  'Batch Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data Load Time {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                  'Top-5 Accuracy {top5.val:.3f} ({top5.avg:.3f})'.format(epoch, i, len(train_loader),
-                                                                          batch_time=batch_time,
-                                                                          data_time=data_time, loss=losses,
-                                                                          top5=top5accs))
